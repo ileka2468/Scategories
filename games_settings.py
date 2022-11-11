6b3c37c0b85f32b461f80eca343d1ed6c72a302f
ileka2468 <ileka2468@gmail.com>
2022-11-10 19:09:25 -0600

    update

--- games_settings.py
+++ games_settings.py
@@ -1,22 +1,46 @@
 # comment check
+from pathlib import Path
 
-def create_categories(csv_list, categories):
-    for csv in csv_list:
+categories = {}
+start_letters = {}
+
+csv_folder = Path("CSV_folder")
+category_csvs = [csv_folder / "fruits.csv", csv_folder / "presidents.csv", csv_folder / "countries.csv"]
+
+def game_settings(rounds: int = 3, ):
+    rounds = input("How many rounds do you want to play?")
+    players = input("How many players are there?")
+    return (rounds, players)
+
+def create_categories():
+    for csv in category_csvs:
         with open(csv, "r") as f:
-            period = csv.find(".")
-            csv = csv[:period].capitalize()
-            categories[csv] = []
+            period = str(csv).find(".")
+            slash = str(csv).find("\\") + 1
+            csv = str(csv)[slash:period].capitalize()
+            categories[str(csv)] = []
             next(f)
             for line in f:
                 item = line.split(",")[0].strip()
-                categories[csv].append(item)
+                categories[csv].append(item.replace('"', ""))
 
-    print(categories)
+    return categories
+
+
+def generate_start_letters():
+    for category_key, category_items in categories.items():
+        start_letters[category_key] = set()
+        for item in category_items:
+            start_letters[category_key].add(item[0])
+
+    return start_letters
+
 
 def main():
-    categories = {}
-    category_csvs = ["fruits.csv", "presidents.csv", "countries.csv"]
-    create_categories(category_csvs, categories)
+    pass
+
+    # create_categories(category_csvs, categories)
+    # generate_start_letters(categories, start_letters)
 
 
 if __name__ == "__main__":
---
+++ playgame.py
@@ -0,0 +1,8 @@
+from games_settings import create_categories, generate_start_letters, game_settings
+
+categories = create_categories()
+start_letters = generate_start_letters()
+settings = game_settings()
+
+print(categories, start_letters, settings)
+
