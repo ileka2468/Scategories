3f39f436590f60cabe1e3dc9a2509c3083c3979a
ileka2468 <ileka2468@gmail.com>
2022-11-14 23:00:42 -0600

    added comments for clarity and round/user input validation

--- GameSettings.py
+++ GameSettings.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import random
 from pytimedinput import timedInput
+from art import*
 
 '''
 This is a final project for CSC 241 at DePaul Univeersity, Professor: Tony Lowe, Group Members: William Ileka, , Fouzan Hussain ,Syed Farhan, Bakhodir Astanov
@@ -19,12 +20,18 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 '''
 
+'''
+initializes two empty maps that will hold category data loaded in from the CSV files, where the key is a slice of the
+file name prior to the extension name and the data WILL be a list of values read in from the CSV.
+'''
+#
 categories = {}
 start_letters = {}
 
 csv_folder = Path("CSV_folder")
 category_csvs = [csv_folder / "fruits.csv", csv_folder / "presidents.csv", csv_folder / "countries.csv", csv_folder / "generic-food.csv"]
 
+# function that writes game info into a file, to be used later
 def game_settings():
     with open("roundinfo.txt", "r") as f:
         line = f.readline()
@@ -33,13 +40,27 @@ def game_settings():
         players = line[comma + 1:]
     return (int(rounds), int(players))
 
+# function that prompts the game creator on the settings for the game
 def getdata():
-    roundz, timeout1 = timedInput("How many rounds per game?: ", timeout=-1, allowCharacters="1,2,3,4,5,6,7,8,9,0")
-    playerz, timeout2 = timedInput("How many people will be playing: ", timeout=-1, allowCharacters="1,2,3,4,5,6,7,8,9,0")
+    welcome = text2art("Scattergories  Menu")
+    print(welcome)
+    while True:
+        roundz, timeout1 = timedInput("How many rounds per game?: ", timeout=-1, allowCharacters="1,2,3,4,5,6,7,8,9,0")
+        playerz, timeout2 = timedInput("How many people will be playing: ", timeout=-1, allowCharacters="1,2,3,4,5,6,7,8,9,0")
+        if int(roundz) > 0 and int(playerz) > 0:
+            break
+        else:
+            print("****Your round number and player must be greater than 0****\n\n")
+    print("\nYour game settings have been save you can now play the game by running PlayGame.py!")
+
     with open("roundinfo.txt","w") as f:
         f.write(f"{roundz},{playerz}")
 
 
+'''
+loops through the csv list and opens each CSV and reads the data into the map created above where the key is the file
+name prior to the period and the value is a list of the data being read in from the CSV file.
+'''
 
 def create_categories():
     for csv in category_csvs:
@@ -54,6 +75,11 @@ def create_categories():
                 categories[csv].append(item.replace('"', ""))
     return categories
 
+'''
+function that loops through the keys and values in the category list and generates a dictionary that maps the category
+name to a valid list of starting letters by, taking the dictionary value which is a list and converting it into a set
+which removes all duplicates.
+'''
 
 def generate_start_letters():
     for category_key, category_items in categories.items():
@@ -62,7 +88,10 @@ def generate_start_letters():
             start_letters[category_key].add(item[0])
 
     return start_letters
-
+'''
+writes the settings file by choosing random categories for each round and writing the information to a file that, the 
+game module will need to play the rounds.
+'''
 
 def write_settingsFile(categories, letters):
     with open("settings.txt", "w") as f:
